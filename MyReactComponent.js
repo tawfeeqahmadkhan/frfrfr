e92ac19634fad58198ffde233c2e3cc0bfc4d84d
tawfeeq <119751359+tawfeeqahmadkhan@users.noreply.github.com>
2024-03-21 12:13:41 +0530

    react

---
+++ MyReactComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyReactComponent() {
+    return (
+        <div>
+            <h2>This is My React Component!</h2>
+            {/* Add your component content here */}
+        </div>
+    );
+}
+
+export default MyReactComponent;
